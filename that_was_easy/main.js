49bbc4a722df994078cd6f14034aa93b03c52a7f
Tri Tran <gocrazylikeamonkey@gmail.com>
2016-07-19 21:08:07 +0000

    other button works

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -9,3 +9,14 @@ function delegateKeypress(event){
         $("easy").trigger("click");
     }
 }
+function sayKidsCheering(){
+var KidsCheering = new Audio("Kids_Cheering_Sound_Effect.mp3");
+KidsCheering.play();
+}
+$("#kc").on("click", sayKidsCheering);
+$(document).keypress(key);
+function key(event){ 
+    if(event.charCode == 33){
+        $("kc").trigger("click");
+    }
+}
