afe2651288d2a8a63d8fc6e2e10abb519c51c5ff
Tri Tran <gocrazylikeamonkey@gmail.com>
2016-07-20 21:54:14 +0000

    i think we done now

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,4 +1,4 @@
-var NUM_CIRCLES = 12;
+var NUM_CIRCLES = 40;
 var circleDiameter;
 var circleRadius;
 var rVal;
@@ -6,14 +6,15 @@ var gVal;
 var bVal;
 
 function setup(){
-    createCanvas(480,600);
+    createCanvas(1270,566);
+    frameRate(6);
     circleDiameter = width/NUM_CIRCLES;
     circleRadius= circleDiameter/2;
-}
-function draw(){
     rVal = 255;
     gVal = 0;
     bVal = 0;
+}
+function draw(){
     var isShifted = false;
     var y = height;
     while(y >= 0){
@@ -25,19 +26,19 @@ function draw(){
         x = 0;
     }
     while (x <= width){
-        fill(color(rVal,gVal,bVal));
         stroke(color(rVal,gVal,bVal));
+        fill(color(rVal,gVal,bVal));
         ellipse(x,y,circleDiameter,circleDiameter);
         x = x + circleDiameter;
         }
         y = y - circleRadius;
         isShifted = !isShifted;
-        rVal = rVal -2;
-        gVal = gVal +7;
-        bVal = bVal +3;
+        rVal = (rVal + 254)%256;
+        gVal = (gVal + 7)%256;
+        bVal = (bVal + 3)%256;
     }
 }
-function KeyPressed(){
+function keyPressed(){
     if(keyCode === 115 || keyCode === 83){
         saveCanvas('geometricPattern','png');
         
