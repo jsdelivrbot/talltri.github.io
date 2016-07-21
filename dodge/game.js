98f96f883930b7b6768fe5b3b68b341848b0c848
Tri Tran <gocrazylikeamonkey@gmail.com>
2016-07-21 23:35:57 +0000

    skhgibgbre

--- dodge/game.js
+++ dodge/game.js
@@ -70,9 +70,9 @@ function draw() {
         if (enemy3.overlap(player)){
             isGameOver = true
         }
-        enemy.position.y = enemy.position.y + 6.5;
-        enemy2.position.x = enemy2.position.x + 8;
-        enemy3.position.x = enemy3.position.x - 8;
+        enemy.position.y = enemy.position.y + 7;
+        enemy2.position.x = enemy2.position.x + 8.6;
+        enemy3.position.x = enemy3.position.x - 8.4;
         drawSprites();
     }
 }
