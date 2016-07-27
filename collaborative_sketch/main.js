fb1b414d1311076dd22cb943c6134c3c15196c0d
Tri Tran <gocrazylikeamonkey@gmail.com>
2016-07-27 22:39:56 +0000

    gr

---
+++ collaborative_sketch/main.js
@@ -0,0 +1,56 @@
+var config = {
+    apiKey: "AIzaSyB1fL_VkUo8l4RvQe5stJv1QDHh4lgc0kc",
+    authDomain: "collaborative-sketch-5d383.firebaseapp.com",
+    databaseURL: "https://collaborative-sketch-5d383.firebaseio.com",
+    storageBucket: "collaborative-sketch-5d383.appspot.com",
+};
+firebase.initializeApp(config);
+var pointsData = firebase.database().ref();
+var points = [];
+
+function drawPointIfMouseIsPressed() {
+    if (mouseIsPressed) {
+        drawPoint();
+    }
+}
+function setup() {
+    var canvas = createCanvas(windowWidth,windowHeight);
+    background(255);
+    fill(0);
+    pointsData.on("child_added", function(point) {
+        points.push(point.val());
+    });
+    pointsData.on("child_removed", function() {
+    points = [];
+})
+        canvas.mousePressed(drawPoint);
+        canvas.mouseMoved(drawPointIfMouseIsPressed);
+}
+function draw() {
+    background(255);
+    for (var i = 0; i < points.length; i++) {
+        var point = points[i];
+        fill(240,29,300);
+        ellipse(point.x, point.y, 15, 15);
+    }
+}
+
+function drawPoint() {
+    pointsData.push({
+        x: mouseX,
+        y: mouseY
+    });
+}
+
+
+$("#saveDrawing").on("click", saveDrawing);
+
+function saveDrawing() {
+    saveCanvas();
+}
+$("#clearDrawing").on("click", clearDrawing);
+
+function clearDrawing() {
+    pointsData.remove();
+    points = [];
+}
